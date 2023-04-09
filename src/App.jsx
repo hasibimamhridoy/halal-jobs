a2fcd0abbdcae0095d4eabb268265c786d9054a2
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-04-10 00:47:28 +0600

    Project Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+     <h1 className='bg-red-400'>Hello</h1>
+    </div>
+  )
+}
+
+export default App

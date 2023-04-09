61c3d085c518b1d84473aaf96230e94eea132f3b
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-04-10 01:43:20 +0600

    Navbar Addded

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li tabIndex={0}>
+                <a className="justify-between">Statistics</a>
+              </li>
+              <li>
+                <a>Applied Jobs</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl">Halal Jobs</a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Home</a>
+            </li>
+            <li tabIndex={0}>
+              <a>Statistics</a>
+            </li>
+            <li>
+              <a>Applied Jobs</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end mt-2">
+          <a className="my-btn">Start Applying</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Header/Navbar/Navbar';
 
 const Layout = () => {
     return (
 
         <div>
             <div className="navbarContainer my-container">
-                
+                <Navbar></Navbar>
             </div>
 
             <div className="outlet my-container">

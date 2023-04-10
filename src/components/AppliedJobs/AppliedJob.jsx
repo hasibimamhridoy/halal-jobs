95c3e5c4eab107629f2c34bd2643e4150f826846
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-04-10 14:22:56 +0600

    filterd functinality added

--- src/components/AppliedJobs/AppliedJob.jsx
+++ src/components/AppliedJobs/AppliedJob.jsx
@@ -1,65 +1,151 @@
-import React from 'react';
-import { useOutletContext } from 'react-router-dom';
-import { CurrencyDollarIcon, MapPinIcon } from '@heroicons/react/24/solid';
+import React, { useState } from "react";
+import { Link, useOutletContext } from "react-router-dom";
+import { CurrencyDollarIcon, MapPinIcon } from "@heroicons/react/24/solid";
 
 const AppliedJob = () => {
-    const [handleApplyNow,applyJob] = useOutletContext()
+  const [handleApplyNow, applyJob, filterbyTime,filterValue,setIsfilter,isFilter] = useOutletContext();
 
-    console.log(applyJob);
-    return (
-        <div>
-            <div className="title my-bg-clr h-52 flex  items-center justify-center">
-          <h1 className="my-title">Applied Jobs</h1>
+  
+console.log(isFilter);
 
-          
+  function handleTrueFalse(){
+    setIsfilter(true)
+  }
+  
 
-        </div>
+  return (
+    <div>
+      <div className="title my-bg-clr h-52 flex  items-center justify-center">
+        <h1 className="my-title">Applied Jobs</h1>
+      </div>
 
-        {applyJob.map((job,i)=>{
-            return(
-                <div
+      <div className="flex justify-end">
+
+        <select onBlur={handleTrueFalse} onChange={(event) =>filterbyTime(event.target.value) } className="select select-secondary rounded-md mt-10  w-[15%] max-w-xs">
+          <option disabled selected>
+            Filter by
+          </option>
+          <option value='Remote' onClick={handleTrueFalse}>Remote</option>
+          <option value='Onsite'>On Site</option>
+         
+        </select>
+      </div>
+
+      <div className="space-y-5 mt-5 pb-10">
+        
+        
+        {!isFilter ? applyJob.map((job, i) => {
+          return (
+            <div
               key={i}
-              className="my-card rounded-md flex flex-col justify-center space-y-1 px-5 py-5 border border-solid border-gray-300"
+              className="my-card rounded-md flex  items-center justify-between space-x-5 space-y-1 px-5 py-5 border border-solid border-gray-300"
             >
-              <div className="icon w-fit rounded-lg">
-                <img
-                  className="w-40 h-12 rounded-md"
-                  src={job.company_logo}
-                  alt=""
-                />
+              <div>
+                <div className="flex items-center space-x-5">
+                  <div className="icon w-fit rounded-lg">
+                    <img
+                      className="w-40 h-12 rounded-md"
+                      src={job.company_logo}
+                      alt=""
+                    />
+                  </div>
+
+                  <div className="py-3">
+                    <h1 className="text-lg">{job.job_title}</h1>
+                    <h1 className="text-sm text-gray-500">
+                      {job.company_name}
+                    </h1>
+                    <div className="full-time flex gap-3 lg:gap-5 mt-3">
+                      <h1 className="border px-6 py-1 rounded-sm text-center border-solid border-blue-600 text-blue-500">
+                        {job.remote_or_onsite}
+                      </h1>
+                      <h1 className="border px-6 py-1 rounded-sm text-center border-solid border-blue-600 text-blue-500">
+                        {job.fulltime_or_parttime}
+                      </h1>
+                    </div>
+
+                    <div className="location-salary flex items-center gap-3 lg-gap-5 mt-3">
+                      <div className="location flex items-center ">
+                        <MapPinIcon className="w-4 h-4"></MapPinIcon>
+                        <h1>{job.location}</h1>
+                      </div>
+                      <div className="salary flex items-center">
+                        <CurrencyDollarIcon className="w-4 h-4"></CurrencyDollarIcon>
+                        <h1>{job.salary}</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
 
-              <div className="py-3">
-                <h1 className="text-lg">{job.job_title}</h1>
-                <h1 className="text-sm text-gray-500">{job.company_name}</h1>
-                <div className="full-time flex gap-3 lg:gap-5 mt-3">
-                  <h1 className="border px-6 py-1 rounded-sm text-center border-solid border-blue-600 text-blue-500">
-                    {job.remote_or_onsite}
-                  </h1>
-                  <h1 className="border px-6 py-1 rounded-sm text-center border-solid border-blue-600 text-blue-500">
-                    {job.fulltime_or_parttime}
-                  </h1>
-                </div>
-
-                <div className="location-salary flex items-center gap-3 lg-gap-5 mt-3">
-                  <div className="location flex items-center ">
-                    <MapPinIcon className="w-4 h-4"></MapPinIcon>
-                    <h1>{job.location}</h1>
-                  </div>
-                  <div className="salary flex items-center">
-                    <CurrencyDollarIcon className="w-4 h-4"></CurrencyDollarIcon>
-                    <h1>{job.salary}</h1>
-                  </div>
-                </div>
-
-                
+              <div>
+                <Link to={`/featuredJobs/${job.id}`}>
+                  <button className="my-btn mt-3">View Details</button>
+                </Link>
               </div>
             </div>
-            )
-          })}
+          );
+        }):
+        
+        filterValue.map((job, i) => {
+          return (
+            <div
+              key={i}
+              className="my-card rounded-md flex  items-center justify-between space-x-5 space-y-1 px-5 py-5 border border-solid border-gray-300"
+            >
+              <div>
+                <div className="flex items-center space-x-5">
+                  <div className="icon w-fit rounded-lg">
+                    <img
+                      className="w-40 h-12 rounded-md"
+                      src={job.company_logo}
+                      alt=""
+                    />
+                  </div>
 
-        </div>
-    );
+                  <div className="py-3">
+                    <h1 className="text-lg">{job.job_title}</h1>
+                    <h1 className="text-sm text-gray-500">
+                      {job.company_name}
+                    </h1>
+                    <div className="full-time flex gap-3 lg:gap-5 mt-3">
+                      <h1 className="border px-6 py-1 rounded-sm text-center border-solid border-blue-600 text-blue-500">
+                        {job.remote_or_onsite}
+                      </h1>
+                      <h1 className="border px-6 py-1 rounded-sm text-center border-solid border-blue-600 text-blue-500">
+                        {job.fulltime_or_parttime}
+                      </h1>
+                    </div>
+
+                    <div className="location-salary flex items-center gap-3 lg-gap-5 mt-3">
+                      <div className="location flex items-center ">
+                        <MapPinIcon className="w-4 h-4"></MapPinIcon>
+                        <h1>{job.location}</h1>
+                      </div>
+                      <div className="salary flex items-center">
+                        <CurrencyDollarIcon className="w-4 h-4"></CurrencyDollarIcon>
+                        <h1>{job.salary}</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <Link to={`/featuredJobs/${job.id}`}>
+                  <button className="my-btn mt-3">View Details</button>
+                </Link>
+              </div>
+            </div>
+          );
+        })
+
+        }
+
+
+      </div>
+    </div>
+  );
 };
 
 export default AppliedJob;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,63 +5,60 @@ import Navbar from "../Header/Navbar/Navbar";
 import { setJobLocalStorage, getJobLocalStorage } from "../Utilities/Utilities";
 
 const Layout = () => {
-
-    
-  const [featuredJobs,setFeaturedJobs] = useState()
-  useEffect(()=>{
-
-    fetch('/featuredJobs.json')
-    .then(res =>res.json())
-    .then(data => setFeaturedJobs(data))
-  },[])
- 
+  const [featuredJobs, setFeaturedJobs] = useState();
+  useEffect(() => {
+    fetch("/featuredJobs.json")
+      .then((res) => res.json())
+      .then((data) => setFeaturedJobs(data));
+  }, []);
 
   const [applyJob, setApplyJob] = useState([]);
 
+  const [filterValue, setFilterValue] = useState([]);
+  const [isFilter,setIsfilter] = useState(false)
 
   const handleApplyNow = (id) => {
     setJobLocalStorage(id);
-    const getStoredItem = getJobLocalStorage()
+    const getStoredItem = getJobLocalStorage();
 
     console.log(getStoredItem);
 
-    const pushItem = []
+    const pushItem = [];
 
     for (const id of getStoredItem) {
-
-        if (featuredJobs) {
-            const findItem = featuredJobs.find(fItem => fItem.id == id)
-        pushItem.push(findItem)
-        }
+      if (featuredJobs) {
+        const findItem = featuredJobs.find((fItem) => fItem.id == id);
+        pushItem.push(findItem);
+      }
     }
 
-    setApplyJob(pushItem)
-
+    setApplyJob(pushItem);
   };
 
-  useEffect(()=>{
-
-    const getStoredItem = getJobLocalStorage()
-
-    console.log(getStoredItem);
-
-    const pushItem = []
+  useEffect(() => {
+    const getStoredItem = getJobLocalStorage();
+    const pushItem = [];
 
     for (const id of getStoredItem) {
-
-        if (featuredJobs) {
-            const findItem = featuredJobs.find(fItem => fItem.id == id)
-        pushItem.push(findItem)
-        }
+      if (featuredJobs) {
+        const findItem = featuredJobs.find((fItem) => fItem.id == id);
+        pushItem.push(findItem);
+      }
     }
 
-    setApplyJob(pushItem)
+    setApplyJob(pushItem);
+   
+  }, [featuredJobs]);
 
-  },[featuredJobs])
+  const filterbyTime = (value) => {
+    const jobs = [...applyJob];
+    const newFilter = jobs.filter((job) => job.remote_or_onsite === value);
+   
+    setFilterValue(newFilter);
+    setIsfilter(true)
+  };
 
-
-
-  
+ 
 
 
 
@@ -72,7 +69,9 @@ const Layout = () => {
       </div>
 
       <div className="outlet ">
-        <Outlet context={[handleApplyNow,applyJob]}></Outlet>
+        <Outlet
+          context={[handleApplyNow, applyJob, filterbyTime,filterValue,setIsfilter,isFilter]}
+        ></Outlet>
       </div>
     </div>
   );

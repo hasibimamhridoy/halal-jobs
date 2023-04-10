4f015c2e259adcf77134a1fbac4f2f0e4b5d7c3c
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-04-10 22:10:39 +0600

    error page done

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import FeaturedJobsDetails from './components/FeaturedJobsDetails/FeaturedJobsDe
 import { ToastContainer, toast } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 function App() {
 
--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { Link, useRouteError } from 'react-router-dom'
+import Lottie from "lottie-react";
+import ErrorLottie from "./98488-bot-error-404.json";
+
 
 const ErrorPage = () => {
-    return (
-        <div>
-            kjdsf;lskfdj;lsf
-        </div>
-    );
-};
+  const { error, status } = useRouteError()
 
-export default ErrorPage;
+  const [isStatus,setIsStatus] = useState(false)
+
+  return (
+    <section className='flex items-center h-screen p-14 bg-gray-100 text-gray-900'>
+        
+      <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+        
+        <div className='max-w-md text-center'>
+          <h2 className='mb-8 font-extrabold text-9xl text-gray-600'>
+            <span className='sr-only'>Error</span> {<Lottie animationData={ErrorLottie} loop={true} /> || status}
+          </h2>
+          <p className='text-2xl font-semibold md:text-3xl mb-8'>
+            {error?.message}
+          </p>
+          <Link
+            to='/'
+            className='px-8 py-3 font-semibold rounded bg-blue-400 text-gray-900'
+          >
+            Back to homepage
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ErrorPage
